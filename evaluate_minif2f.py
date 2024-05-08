515dbf8534965fce12a45ab220bfefd394157b30
andrewmilas10 <andrewmilas10@gmail.com>
2024-05-08 12:23:16 -0400

    Update with new results

--- evaluate_minif2f.py
+++ evaluate_minif2f.py
@@ -36,7 +36,8 @@ for name in os.listdir(f"data/full_data/{data_split}"):
     problem_names.append((path, len(context["informal_proof"]), context))
 
 from random import shuffle
-problem_names = sorted(problem_names, key=lambda x: x[1])[7:]
+restart_from = 0
+problem_names = sorted(problem_names, key=lambda x: x[1])[restart_from:]
 # shuffle(problem_names)
 for i, (path, proof_len, _) in enumerate(problem_names):
     print(f"{i + 1}: {proof_len} chars: {path}")
@@ -47,10 +48,10 @@ agent = StepByStepAgent(logger=logger)
 corrects = 0
 for i, (path, proof_len, _) in enumerate(problem_names):
     context = U.load_json(path)
-    solved, proof = agent.attempt_problem(context, path, i)
+    solved, proof = agent.attempt_problem(context, path, i+restart_from)
     print("HIII", solved)
     if solved:
         corrects += 1
         with open(f"results/found_proofs-{data_split}-{start_time}.txt", "a") as f:
-            f.write(f"{i}: {path} of informal length {proof_len} proved with sol of length {len(proof)}\n")
-    print(f"Correct {corrects} out of {i+1}. Ratio: {corrects/(i+1)}\n")
+            f.write(f"{i+restart_from}: {path} of informal length {proof_len} proved with sol of length {len(proof)}\n")
+    print(f"Correct {corrects} out of {i+restart_from+1}. Ratio: {corrects/(i+restart_from+1)}\n")
